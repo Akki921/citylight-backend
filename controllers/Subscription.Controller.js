48566282f6f3643e030465ebbdb74a5505637271
Akki921 <akshayclg36@gmail.com>
2022-01-21 20:05:47 +0530

    solved product issue

--- controllers/Subscription.Controller.js
+++ controllers/Subscription.Controller.js
@@ -1,52 +1,100 @@
 const Subscription = require("../models/Subscription");
 module.exports = {
-  createSubscription: async (SubscriptionData) => {
+  // createSubscription: async (SubscriptionData) => {
 
+  //   return new Promise(async (resolve) => {
+  //     try {
+  // console.log('SubscriptionData',SubscriptionData);
+  //      Subscription.findOne({ subNo: SubscriptionData.no }).exec(
+  //         (err, data) => {
+  //           if (data) {
+  //             console.log("update");
+  //             return resolve({
+  //               status: true,
+  //               message: "subscription is already insert !",
+  //               data: data,
+  //             });
+  //           } else {
+  //             console.log("create");
+  //             var newSubscription = new Subscription({
+  //               subNo:SubscriptionData.subNo,
+  //               subDate:SubscriptionData.subDate,
+  //               startFrom:SubscriptionData.startFrom,
+  //               order:SubscriptionData.orderNo,
+  //               QtyperDay:SubscriptionData.QtyperDay,
+  //               frequency:SubscriptionData.frequency,
+  //               endDate:SubscriptionData.endDate,
+  //               iscancle:SubscriptionData.iscancle,
+  //               // nosubscriptionFrom:SubscriptionData.nosubscriptionFrom,
+  //               // nosubscriptionTo:SubscriptionData.nosubscriptionTo
+  //             });
+  //             newSubscription.save(async (error, Subscription) => {
+  //               console.log("Subscription,",Subscription);
+
+  //               if (error)
+  //                 return resolve({
+  //                   status: false,
+  //                   message: "Please try after some time",
+  //                 });
+  //               if (Subscription) {
+  //                 return resolve({
+  //                   status: true,
+  //                   data: Subscription,
+  //                   message: "Subscription has been created",
+  //                 });
+  //               }
+  //             });
+  //           }
+  //         }
+  //       );
+  //     } catch (error) {
+  //       return resolve({
+  //         status: false,
+  //         message: "Please try after some time" + error,
+  //       });
+  //     }
+  //   });
+  // },
+
+  createSubscription: async (SubscriptionData) => {
+    console.log('SubscriptionData',SubscriptionData);
     return new Promise(async (resolve) => {
+   // console.log(SubscriptionData);
       try {
-  console.log('SubscriptionData',SubscriptionData);
-       Subscription.findOne({ subNo: SubscriptionData.no }).exec(
-          (err, data) => {
-            if (data) {
-              console.log("update");
-              return resolve({
-                status: true,
-                message: "subscription is already insert !",
-                data: data,
-              });
-            } else {
-              console.log("create");
-              var newSubscription = new Subscription({
-                subNo:SubscriptionData.subNo,
-                subDate:SubscriptionData.subDate,
-                startFrom:SubscriptionData.startFrom,
-                order:SubscriptionData.orderNo,
-                QtyperDay:SubscriptionData.QtyperDay,
-                frequency:SubscriptionData.frequency,
-                endDate:SubscriptionData.endDate,
-                iscancle:SubscriptionData.iscancle,
-                // nosubscriptionFrom:SubscriptionData.nosubscriptionFrom,
-                // nosubscriptionTo:SubscriptionData.nosubscriptionTo
-              });
-              newSubscription.save(async (error, Subscription) => {
-                console.log("Subscription,",Subscription);
-                
-                if (error)
-                  return resolve({
-                    status: false,
-                    message: "Please try after some time",
-                  });
-                if (Subscription) {
-                  return resolve({
-                    status: true,
-                    data: Subscription,
-                    message: "Subscription has been created",
-                  });
-                }
-              });
-            }
+        Subscription.findOne({subNo: SubscriptionData.no  }).exec((err, data) => {
+          if (data) {
+            return resolve({
+              status: true,
+              message: "Subscription is already insert !",
+              data: data,
+            });
+          } else {
+            var newSubscription = new Subscription({
+              subNo:SubscriptionData.subNo,
+              subDate:SubscriptionData.subDate,
+              startFrom:SubscriptionData.startFrom,
+              order:SubscriptionData.id,
+              QtyperDay:SubscriptionData.QtyperDay,
+              frequency:SubscriptionData.frequency,
+              endDate:SubscriptionData.enddate,
+            });
+            newSubscription.save(async (error, Subscription) => {
+              console.log(Subscription);
+              if (error)
+                return resolve({
+                  status: false,
+                  message: "Please try after some time",
+                });
+              if (Subscription) {
+                return resolve({
+                  status: true,
+                  data: Subscription,
+                  message: "Subscription has been created",
+                });
+              }
+            });
           }
-        );
+        });
       } catch (error) {
         return resolve({
           status: false,
@@ -90,7 +138,7 @@ module.exports = {
     return new Promise(async (resolve) => {
         try {
             Subscription.find({})
-              .populate("order", " orderNo customer orderDate orderValue coupan orderStatus startDate product ")
+              .populate("order", " orderNo customer orderDate orderValue coupan orderStatus startDate product slottime")
               .exec((error, data) => {
                
                 if (error)

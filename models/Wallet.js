d1c1621be9cec1b789e9f128a8df35bee5fef696
Akki921 <akshayclg36@gmail.com>
2022-02-11 13:29:37 +0530

    sd

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const walletSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'CustomerLogin', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'CustomerProfile', required: true },
     FirstName:{type:String},
     Mobile:{type:Number},
     availableBalance:{type: Number, default: 0}

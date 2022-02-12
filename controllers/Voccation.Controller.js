8d53861ae0bbe7e6538125060157cb1574dd1ed2
Akki921 <akshayclg36@gmail.com>
2022-02-12 20:25:21 +0530

    update voccation

--- controllers/Voccation.Controller.js
+++ controllers/Voccation.Controller.js
@@ -4,54 +4,62 @@ module.exports = {
     console.log(VoccationData);
     return new Promise(async (resolve) => {
       try {
-        Voccation.findOne({ ProfileId: VoccationData.ProfileId }).exec((err, data) => {
-          if (data) {
-            Voccation.findOneAndUpdate(
+        Voccation.findOne({ ProfileId: VoccationData.ProfileId }).exec(
+          (err, data) => {
+            if (data) {
+              Voccation.findOneAndUpdate(
                 { _id: data._id },
                 {
-                       ProfileId: VoccationData.ProfileId,
-                StartDate: VoccationData.StartDate,
-                EndDate: VoccationData.EndDate,
+                  ProfileId: VoccationData.ProfileId,
+                  StartDate: VoccationData.StartDate,
+                  EndDate: VoccationData.EndDate,
+                  city: VoccationData.city,
+                  locality: VoccationData.locality,
+                  cutOffTime: VoccationData.cutOffTime,
                 },
                 { new: true, upsert: true }
-              ).exec((err, data) => { 
+              ).exec((err, data) => {
                 if (data) {
-                    return resolve({
-                      status: true,
-                      message: "Voccation details  is updated !",
-                      data: data,
-                    });
-                  } else if (err) {
-                    return resolve({
-                      status: false,
-                      message: "Voccation details is updating failed !",
-                      data: data,
-                    });
-                  }
-                });
-          } else {
-            console.log("create");
-            var newVoccation = new Voccation({
+                  return resolve({
+                    status: true,
+                    message: "Voccation details  is updated !",
+                    data: data,
+                  });
+                } else if (err) {
+                  return resolve({
+                    status: false,
+                    message: "Voccation details is updating failed !",
+                    data: data,
+                  });
+                }
+              });
+            } else {
+              console.log("create");
+              var newVoccation = new Voccation({
                 ProfileId: VoccationData.ProfileId,
                 StartDate: VoccationData.StartDate,
                 EndDate: VoccationData.EndDate,
-            });
-            newVoccation.save(async (error, brand) => {
-              if (error)
-                return resolve({
-                  status: false,
-                  message: "Please try after some time",
-                });
-              if (brand) {
-                return resolve({
-                  status: true,
-                  data: brand,
-                  message: "Voccation has been created",
-                });
-              }
-            });
+                city: VoccationData.city,
+                locality: VoccationData.locality,
+                cutOffTime: VoccationData.cutOffTime,
+              });
+              newVoccation.save(async (error, brand) => {
+                if (error)
+                  return resolve({
+                    status: false,
+                    message: "Please try after some time",
+                  });
+                if (brand) {
+                  return resolve({
+                    status: true,
+                    data: brand,
+                    message: "Voccation has been created",
+                  });
+                }
+              });
+            }
           }
-        });
+        );
       } catch (error) {
         return resolve({
           status: false,
@@ -61,22 +69,26 @@ module.exports = {
     });
   },
   getVoccationProfileID: async (ProfileId) => {
-      console.log('data',ProfileId)
+    console.log("data", ProfileId);
     return new Promise(async (resolve) => {
-      try {
-        Voccation.find({ "ProfileId": {"_id":ProfileId}}, async (err, data) => {
-          if (err)
-            return resolve({
-              status: false,
-              message: "Please try after some time" + err,
-            });
-          if (data)
-            return resolve({
-              status: true,
-              data: data,
-              message: "Voccation retrieved successfully",
-            });
+      try { 
+      Voccation.find({ ProfileId: { _id: ProfileId }})
+      .populate("ProfileId", "slottime")
+      .populate("locality", "locality availability morning evening morningtime eveningtime")
+      .populate("city", "cityName")
+      .exec((error, data) => {
+      if (error)
+        return resolve({
+          status: false,
+          message: "Please try after some time",
         });
+      if (data)
+        return resolve({
+          status: true,
+          data: data,
+          message: "customer profile retrieved successfully",
+        });
+    });
       } catch (error) {
         return resolve({
           status: false,
@@ -89,19 +101,23 @@ module.exports = {
   getAllVoccation: async () => {
     return new Promise(async (resolve) => {
       try {
-        Voccation.find({}, async (err, data) => {
-          if (err)
-            return resolve({
-              status: false,
-              message: "Please try after some time",
-            });
-          if (data)
-            return resolve({
-              status: true,
-              data: data,
-              message: "Voccation retrieved successfully",
-            });
-        });
+        Voccation.find({})
+      .populate("ProfileId", "slottime")
+      .populate("locality", "locality availability morning evening morningtime eveningtime")
+      .populate("city", "cityName")
+        .exec((error, data) => {
+          if (error)
+          return resolve({
+            status: false,
+            message: "Please try after some time "+error,
+          });
+        if (data)
+          return resolve({
+            status: true,
+            data: data,
+            message: "Voccation retrieved successfully",
+          });
+      });
       } catch (error) {
         return resolve({
           status: false,
@@ -111,40 +127,40 @@ module.exports = {
     });
   },
 
-//   editupdateBrand: async (BrandData) => {
-//     console.log(BrandData)
-//     return new Promise(async (resolve) => {
-//       try {
-//         let rests = Brand.findOneAndUpdate(
-//           { _id: BrandData._id },
-//           {
-//             BrandName: BrandData.BrandName,
-//             Manufacture: BrandData.Manufacture,
-//             isActive: BrandData.isActive,
-//             comment: BrandData.comment,
-//           },
-//           { new: true, upsert: true }
-//         ).exec((err, data) => {
-//           if (data) {
-//             return resolve({
-//               status: true,
-//               message: "Brand is updated !",
-//               data: data,
-//             });
-//           } else if (err) {
-//             return resolve({
-//               status: false,
-//               message: "Brand updating failed !",
-//               data: data,
-//             });
-//           }
-//         });
-//       } catch (error) {
-//         return resolve({
-//           status: false,
-//           message: "Please try after some time" + e,
-//         });
-//       }
-//     });
-//   },
+  //   editupdateBrand: async (BrandData) => {
+  //     console.log(BrandData)
+  //     return new Promise(async (resolve) => {
+  //       try {
+  //         let rests = Brand.findOneAndUpdate(
+  //           { _id: BrandData._id },
+  //           {
+  //             BrandName: BrandData.BrandName,
+  //             Manufacture: BrandData.Manufacture,
+  //             isActive: BrandData.isActive,
+  //             comment: BrandData.comment,
+  //           },
+  //           { new: true, upsert: true }
+  //         ).exec((err, data) => {
+  //           if (data) {
+  //             return resolve({
+  //               status: true,
+  //               message: "Brand is updated !",
+  //               data: data,
+  //             });
+  //           } else if (err) {
+  //             return resolve({
+  //               status: false,
+  //               message: "Brand updating failed !",
+  //               data: data,
+  //             });
+  //           }
+  //         });
+  //       } catch (error) {
+  //         return resolve({
+  //           status: false,
+  //           message: "Please try after some time" + e,
+  //         });
+  //       }
+  //     });
+  //   },
 };
--- models/Voccation.js
+++ models/Voccation.js
@@ -7,7 +7,17 @@ const VoccationSchema = new mongoose.Schema({
     required: true,
   },
   StartDate: { type: Date, required: true },
-  EndDate: { type: Date },
+  city: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "City",
+  },
+  locality: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "CityAvailability",
+  },
+  cutOffTime: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model("Voccation", VoccationSchema);

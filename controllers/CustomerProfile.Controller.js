16193f328fedc5c07a6553159b6a997f7c90d8ee
Akki921 <akshayclg36@gmail.com>
2022-02-18 17:36:19 +0530

    update

--- controllers/CustomerProfile.Controller.js
+++ controllers/CustomerProfile.Controller.js
@@ -1,16 +1,14 @@
-
-const CustomerProfile= require("../models/CustomerProfile");
+const CustomerProfile = require("../models/CustomerProfile");
 const CityAvailability = require("../models/CityAvailability");
 module.exports = {
-
   // createCustomerProfile: async (Data) => {
   //   return new Promise(async (resolve) => {
   //     console.log(Data);
   //     try {
   //       CustomerProfile.findOne({"login": Data.login }
   //        )
-  //         .exec((err, data) => {         
-           
+  //         .exec((err, data) => {
+
   //           if (data)
   //           {
   //             return resolve({
@@ -56,53 +54,53 @@ module.exports = {
   //   });
   // },
 
-//   getCityByID: async (data) => {
-//     return new Promise(async (resolve) => {
-//       try {
-//         City.findById({_id:data}, async (err, data) => {
-//           if (err)
-//             return resolve({
-//               status: false,
-//               message: "Please try after some time"+err,
-              
-//             });
-//           if (data)
-//             return resolve({
-//               status: true, 
-//               data: data,
-//               message: "Data retrieved successfully",
-//             });
-            
-//         });
-//       } catch (error) {
-//         return resolve({
-//           status: false,
-//           message: "Please try after some time2"+error,
-//         });
-//       }
-//     });
-//   },
+  //   getCityByID: async (data) => {
+  //     return new Promise(async (resolve) => {
+  //       try {
+  //         City.findById({_id:data}, async (err, data) => {
+  //           if (err)
+  //             return resolve({
+  //               status: false,
+  //               message: "Please try after some time"+err,
+
+  //             });
+  //           if (data)
+  //             return resolve({
+  //               status: true,
+  //               data: data,
+  //               message: "Data retrieved successfully",
+  //             });
+
+  //         });
+  //       } catch (error) {
+  //         return resolve({
+  //           status: false,
+  //           message: "Please try after some time2"+error,
+  //         });
+  //       }
+  //     });
+  //   },
 
   getProfiles: async () => {
     return new Promise(async (resolve) => {
       try {
         CustomerProfile.find({})
-          .populate("login","phone")
-          .populate("city","cityName")
-          .populate("locality","locality")
+          .populate("login", "phone")
+          .populate("city", "cityName")
+          .populate("locality", "locality")
           .exec((err, data) => {
-          if (err)
-            return resolve({
-              status: false,
-              message: "Please try after some time",
-            });
-          if (data)
-            return resolve({
-              status: true,
-              data: data,
-              message: "Data retrieved successfully",
-            });
-        });
+            if (err)
+              return resolve({
+                status: false,
+                message: "Please try after some time",
+              });
+            if (data)
+              return resolve({
+                status: true,
+                data: data,
+                message: "Data retrieved successfully",
+              });
+          });
       } catch (error) {
         return resolve({
           status: false,
@@ -112,54 +110,53 @@ module.exports = {
     });
   },
 
-//   
-createcProfile: async (Data) => {
+  //
+  createcProfile: async (Data) => {
     return new Promise(async (resolve) => {
       console.log(Data);
       try {
         CustomerProfile.findOne({ login: Data.login }).exec((err, data) => {
-          if (data) { 
+          if (data) {
             CustomerProfile.findOneAndUpdate(
-                { _id: data._id },
-                {
-                    login: data.login,
-                    username: data.username,
-                    phone: data.phone,
-                    houseno: data.houseno,
-                    address: data.address,
-                    city: data.city,
-                    locality: data.locality,
-                    ringtheBell: data.ringtheBell,
-                    slottime: data.slottime,
-                },
-                { new: true, upsert: true }
-              ).exec((err, data) => { 
-                if (data) {
-                    return resolve({
-                      status: true,
-                      message: "login details  is updated !",
-                      data: data,
-                    });
-                  } else if (err) {
-                    return resolve({
-                      status: false,
-                      message: "login details is updating failed !",
-                      data: data,
-                    });
-                  }
+              { _id: data._id },
+              {
+                login: data.login,
+                username: data.username,
+                phone: data.phone,
+                houseno: data.houseno,
+                address: data.address,
+                city: data.city,
+                locality: data.locality,
+                ringtheBell: data.ringtheBell,
+                slottime: data.slottime,
+              },
+              { new: true, upsert: true }
+            ).exec((err, data) => {
+              if (data) {
+                return resolve({
+                  status: true,
+                  message: "login details  is updated !",
+                  data: data,
                 });
+              } else if (err) {
+                return resolve({
+                  status: false,
+                  message: "login details is updating failed !",
+                  data: data,
+                });
+              }
+            });
           } else {
-           
             var newCustomerProfile = new CustomerProfile({
-                login: Data.login,
-                username: Data.username,
-                houseno: Data.houseno,
-                phone: Data.phone,
-                address: Data.address,
-                city: Data.city,
-                locality: Data.locality,
-                ringtheBell: Data.ringtheBell,
-                slottime: Data.slottime,
+              login: Data.login,
+              username: Data.username,
+              houseno: Data.houseno,
+              phone: Data.phone,
+              address: Data.address,
+              city: Data.city,
+              locality: Data.locality,
+              ringtheBell: Data.ringtheBell,
+              slottime: Data.slottime,
             });
             newCustomerProfile.save(async (error, Profile) => {
               if (error)
@@ -168,7 +165,6 @@ createcProfile: async (Data) => {
                   message: "Please try after some time",
                 });
               if (Profile) {
-              
                 return resolve({
                   status: true,
                   data: Profile,
@@ -191,23 +187,25 @@ createcProfile: async (Data) => {
     console.log(id);
     return new Promise(async (resolve) => {
       try {
-
-        CustomerProfile.find({ "login": { "_id":id, }})
+        CustomerProfile.find({ login: { _id: id } })
           .populate("city", "id cityName")
-    .populate("locality", "id locality availability morning morningtime evening eveningtime")
+          .populate(
+            "locality",
+            "id locality availability morning morningtime evening eveningtime"
+          )
           .exec((error, data) => {
-          if (error)
-            return resolve({
-              status: false,
-              message: "Please try after some time",
-            });
-          if (data)
-            return resolve({
-              status: true,
-              data: data,
-              message: "customer profile retrieved successfully",
-            });
-        });
+            if (error)
+              return resolve({
+                status: false,
+                message: "Please try after some time",
+              });
+            if (data)
+              return resolve({
+                status: true,
+                data: data,
+                message: "customer profile retrieved successfully",
+              });
+          });
       } catch (error) {
         return resolve({
           status: false,

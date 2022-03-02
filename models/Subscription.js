a656e2ed339e8b8dca080060a42368b26e008fe4
Akki921 <akshayclg36@gmail.com>
2022-03-02 19:00:39 +0530

    c

--- models/Subscription.js
+++ models/Subscription.js
@@ -32,7 +32,7 @@ const subscriptionSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "CityAvailability",
   },
-  customDates: [{ Start: Date }],
+  customDates: [{ Start: Date,qtyoftheday: Number }],
   QtyperDay: {
     type: Number,
   },
